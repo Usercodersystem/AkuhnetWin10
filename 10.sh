267e0dcd5b45792000620f32423b5754a4c5fe93
Usercodersystem <70068765+Usercodersystem@users.noreply.github.com>
2022-12-26 19:02:16 +0000

    Add files via upload

--- 10.sh
+++ 10.sh
@@ -1,36 +1,41 @@
-echo "===================================="
+apt-get update
 echo "Download windows files"
-echo "===================================="
-curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
-echo "===================================="
-echo "Download ngrok"
-echo "===================================="
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip &>/dev/null &
-unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
-./ngrok authtoken 1lc6OFq3p80TAl5fEbY50VjPrS8_XRqnQJQ9mPv7KGyxigT3
-nohup ./ngrok tcp 3389 &>/dev/null &
+wget -O w10x64.img https://bit.ly/akuhnetW10x64
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
 ./ngrok tcp 3389 &>/dev/null &
-echo "===================================="
+clear
 echo Downloading File From akuh.net
-echo "===================================="
-apt-get install qemu > /dev/null 2>&1
-echo "===================================="
+apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
-echo "===================================="
-echo "===================================="
+qemu-system-x86_64 -hda w10x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "===================================="
-echo "Ctrl+C To Copy"
-echo "Wait 1-2 minute to finish bot"
-echo "Dont Close This Tab"
-echo "===================================="
 echo "===================================="
 echo "Username: akuh"
 echo "Password: Akuh.Net"
 echo "===================================="
-qemu-system-x86_64 -hda w10x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic  > /dev/null 2>&1
-sleep 43200
+echo "===================================="
+echo "Keep supporting akuh.net, thank you"
+echo "You Got Free RDP now"
+echo "Wait 2 minute to finish bot"
+echo "You can close this tab"
+echo "RDP runs for 50 hours"
+echo "===================================="
+sleep 432000
---
+++ ng.sh
@@ -0,0 +1,7 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken 1lc6OFq3p80TAl5fEbY50VjPrS8_XRqnQJQ9mPv7KGyxigT3
